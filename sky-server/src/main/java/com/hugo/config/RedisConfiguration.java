4cdca5d4b190d55b356cf318a25b7f79f1e583ee
HugoSwen <2407475450@qq.com>
2023-09-30 22:57:02 +0800

    完善SpringCache的序列化方式和配置

--- sky-server/src/main/java/com/hugo/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/hugo/config/RedisConfiguration.java
@@ -3,15 +3,12 @@ package com.hugo.config;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -26,19 +23,14 @@ public class RedisConfiguration {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         log.info("开始创建redis模板类");
 
-        FastJsonRedisSerializer<Object> serializer = new FastJsonRedisSerializer<>(Object.class);
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
-        serializer.setFastJsonConfig(fastJsonConfig);
-
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         // key的序列化
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         // value的序列化
-        redisTemplate.setValueSerializer(new FastJsonRedisSerializer<>(Object.class));
-        redisTemplate.setHashValueSerializer(new FastJsonRedisSerializer<>(Object.class));
+        redisTemplate.setValueSerializer(new GenericFastJsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericFastJsonRedisSerializer());
 
         return redisTemplate;
     }
